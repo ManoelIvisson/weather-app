67468bcf179834749cc947dc65c5961ee98ec947
Manoel <manoelivisson4@hotmail.com>
2025-05-27 19:07:19 -0300

    Criando projeto de app de clima

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <main>
+        <div id='pesquisa'>
+          <form action="">
+            <input type="text" name="cidade" id="cidade" placeholder='Digite o nome da cidade... ' />
+          </form>
+        </div>
+
+        <div>
+          <h1 id='nome-cidade'></h1>
+          
+        </div>
+
+      </main>
+    </>
+  )
+}
+
+export default App
